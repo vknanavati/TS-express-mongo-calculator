1748cb5fe5d53bac25df85cb387e110c93dbbeb6
vknanavati <vimin.nanavati@gmail.com>
2025-03-25 15:09:56 -0400

    created register form.

---
+++ my-budget-app/src/components/Register.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+
+export function Register() {
+    useEffect(()=>{
+        console.log("rendering register page")
+    }, [])
+    return (
+        <div>
+            Hello
+        </div>
+  )
+}
+
+
